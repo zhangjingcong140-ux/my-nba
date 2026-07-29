3667be79e82909711bb34091389ee49c7dea2299
zhangjingcong140-ux <zhangjingcong140@gmail.com>
2026-07-29 20:10:44 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,22 @@
+from player import Player
+def load_players():
+    players=[]
+    try:
+        with open("players.txt","r")as f:
+            for line in f:
+                name,age,team,rating,position=line.strip().split(",")
+                player=Player(name,int(age),team,int(rating),position)
+                players.append(player)
+    except FileNotFoundError:
+        print("File not found.")
+    return players
+
+def save_players(players):
+    with open("players.txt","w")as f:
+        for player in players:
+            f.write(f"{player.name},{player.age},{player.team},{player.rating},{player.position}\n")
+def find_player(players,name):
+    for player in players:
+        if player.name==name:
+            return player
+    return None
